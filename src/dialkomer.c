0d7919c9b1b5db7355af81129fcf23f607ae7a81
Smadas <adam.sojka@smadas.net>
2016-11-30 21:37:45 +0100

    Vytvorenie kniznice dialkomer
    Vytvorenie zdrojoveho suboru dialkomer.c a prislusneho hlavickoveho
    suboru.

---
+++ src/dialkomer.c
@@ -0,0 +1,17 @@
+/*
+ * dialkomer.c
+ *
+ *  Created on: Nov 30, 2016
+ *      Author: Adam Sojka
+ */
+
+
+#include <stddef.h>
+#include <string.h>
+#include "stm32l1xx.h"
+#include "dialkomer.h"
+
+int meraj_dialkomer(int cislo_senzora)
+{
+
+}
---
+++ src/dialkomer.h
@@ -0,0 +1,13 @@
+/*
+ * dialkomer.h
+ *
+ *  Created on: Nov 30, 2016
+ *      Author: Adam Sojka
+ */
+
+#ifndef DIALKOMER_H_
+#define DIALKOMER_H_
+
+
+
+#endif /* DIALKOMER_H_ */
